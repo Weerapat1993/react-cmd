20286de26467b0db08e25ade4d62759a6dbc5e22
Top <weerapat.thawatchoketawee@gmail.com>
2018-01-06 15:04:05 +0700

    remove comment

--- app/commands/install.js
+++ app/commands/install.js
@@ -12,25 +12,6 @@ const Install = (cmd, env) => {
     .copyFolderTemplate('/views', '/views')
     .copyFile('/index.js', '/index.js')
     .copyFile('/registerServiceWorker.js', '/registerServiceWorker.js')
-//     .createFile('/index.js',
-// `import React from 'react'
-// import ReactDOM from 'react-dom'
-// import { Provider } from 'react-redux'
-// import { Routes } from './views/routes'
-// import { RouterMiddleware } from './utils'
-// import configureStore from './redux/store'
-// import registerServiceWorker from './registerServiceWorker'
-
-// ReactDOM.render(
-//   <Provider store={configureStore()}>
-//     <RouterMiddleware Routes={Routes} />
-//   </Provider>
-// ,document.getElementById('root'))
-
-// registerServiceWorker()
-
-// `
-// )
 }
 
 module.exports = Install
