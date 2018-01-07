04bea7d067531dc8d9ca87d5fe73ab7a4783bc47
Top <weerapat.thawatchoketawee@gmail.com>
2018-01-07 18:48:38 +0700

    refactor code

--- src/views/components/App.jsx
+++ src/views/components/App.jsx
@@ -1,27 +1,18 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { authActions } from '../../redux/auth'
+import { Aunthentication } from './Aunthentication'
 import { Header } from './Header'
 import { LeftDrawer } from './LeftDrawer'
 import withWidth, {LARGE, SMALL} from 'material-ui/utils/withWidth'
 import Data from '../assets/data/data'
 
 class App extends Component {
-
   constructor(props) {
     super(props)
     this.state = {
       navDrawerOpen: props.width === LARGE
     }
-
-    this.handleLogout = this.handleLogout.bind(this)
     this.handleChangeRequestNavDrawer = this.handleChangeRequestNavDrawer.bind(this)
-  }
-
-  handleLogout() {
-    this.props.authActions.authLogout()
   }
 
   componentWillReceiveProps(nextProps) {
@@ -38,7 +29,7 @@ class App extends Component {
 
   render() {
     let { navDrawerOpen } = this.state
-    const { auth, width } = this.props
+    const { auth, authLogout, width } = this.props
     const isMobile = width <= 480
     const paddingLeftDrawerOpen = 236
 
@@ -59,7 +50,7 @@ class App extends Component {
           navDrawerOpen={navDrawerOpen && !isMobile}
           handleChangeRequestNavDrawer={this.handleChangeRequestNavDrawer}
           auth={auth}
-          onLogout={this.handleLogout}
+          onLogout={authLogout}
         />
 
         <LeftDrawer 
@@ -77,18 +68,20 @@ class App extends Component {
 
 App.propTypes = {
   children: PropTypes.element,
-  width: PropTypes.number
+  width: PropTypes.number,
+  auth: PropTypes.shape({
+    isAuth: PropTypes.bool,
+    isLoading: PropTypes.bool,
+    user: PropTypes.oneOfType([
+      PropTypes.bool,
+      PropTypes.object,
+    ]),
+    error: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.bool,
+    ])
+  }),
+  authLogout: PropTypes.func.isRequired,
 }
 
-const mapStateToProps = (state, ownProps) => ({
-  auth: state.auth
-})
-
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  authActions: bindActionCreators(authActions, dispatch)
-})
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(withWidth()(App)) 
+export default withWidth()(Aunthentication(App))
